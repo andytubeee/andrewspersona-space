f0bf8d2b7a1f40ac08d7e062d2f03c71d8789511
Andrew Yang <andy.tubeee@gmail.com>
2021-07-29 22:19:33 -0400

    Trial 1: Protection system

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// alert(
-//   "I have implemented 0 CSS on this page, things will look unpleasant. Viewer's discretion is advised"
-// );
-// confirm('Enter the site?');
-
 const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 document.getElementById('mySite_anchor').addEventListener('click', function () {
