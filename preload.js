cbb1ca822b29c460fcd65ae6a7a28977d549cc52
Andrew Yang <andy.tubeee@gmail.com>
2021-07-29 22:21:00 -0400

    Bolded unvisit page

--- preload.js
+++ preload.js
@@ -2,5 +2,5 @@ alert(
   "I have implemented 0 CSS on this page, things will look unpleasant. Viewer's discretion is advised"
 );
 if (!confirm('Enter the site?')) {
-  document.body.innerHTML = 'Understandable, have a great day!';
+  document.body.innerHTML = '<h1>Understandable, have a great day!</h1>';
 }
