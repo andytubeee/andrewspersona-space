aa11ac12e28686ac81bb8a7c843879defb0a3b56
Andrew Yang <andy.tubeee@gmail.com>
2021-07-29 22:48:31 -0400

    Hide site feature

--- preload.js
+++
@@ -1,6 +0,0 @@
-alert(
-  "I have implemented 0 CSS on this page, things will look unpleasant. Viewer's discretion is advised"
-);
-if (!confirm('Enter the site?')) {
-  document.body.innerHTML = '<h1>Understandable, have a great day!</h1>';
-}
